80b55e88864b9655b15ce18f9b60368d507b7d96
Mo-ra778 <mohammedalidrisi001@gmail.com>
2026-08-30 02:02:55 +0300

    fix(fatal): import Log facade to prevent Class Log not found 500 error in web controllers

--- ewallet-backend/app/Http/Controllers/Web/AgentWebController.php
+++ ewallet-backend/app/Http/Controllers/Web/AgentWebController.php
@@ -12,6 +12,7 @@ use App\Services\PushNotificationService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 
 class AgentWebController extends Controller
 {
